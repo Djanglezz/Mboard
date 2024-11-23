68c9232294f47b0ea65463fb7ed5ad683b9adc15
Max Klein <98134261+Djanglezz@users.noreply.github.com>
2024-11-23 13:46:36 -0700

    Add files via upload

---
+++ public/secrets.js
@@ -0,0 +1,4 @@
+/* App secrets */
+export const apiKey = "18b23308-1804-45ae-bb4d-dcb21321fbf0";
+export const itemId = "6bc42939-398c-4542-bc8f-04d42e4d5663";
+export const userId = "ef6bfca2-0590-4a29-a3c7-fce9ca0ade1a";
